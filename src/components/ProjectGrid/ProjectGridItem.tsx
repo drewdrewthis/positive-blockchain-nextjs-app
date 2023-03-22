b45946a21fa1088b410f87146dda69135819216e
drewdrew <drewdrewthis@gmail.com>
2023-03-22 13:29:55 +0100

    Introduce basic filtering

--- src/components/ProjectGrid/ProjectGridItem.tsx
+++ src/components/ProjectGrid/ProjectGridItem.tsx
@@ -21,8 +21,12 @@ function ProjectGridItem(props: Props) {
     thumbnailSrc = "https://positiveblockchain.io/wp-content/uploads/2019/01/12642539_740603619408591_4005717202573682653_n.png",
   } = props;
 
+  const formattedDescription = description
+    ? description[0].toUpperCase() + description.slice(1)
+    : description;
+
   return (
-    <Grid xs={12} sm={6} md={4}>
+    <Grid xs={12} sm={6} md={4} lg={3}>
       <div
         className={cx(
           "flex flex-col h-full rounded border bg-white",
@@ -30,20 +34,20 @@ function ProjectGridItem(props: Props) {
         )}
       >
         <div className="flex items-center gap-2 p-3">
-          <Image
+          {/* <Image
             className="m-0"
             src={thumbnailSrc}
             alt={name}
             width={75}
             height={75}
-          />
+          /> */}
           <div>
-            <h4 className="font-bold">{name}</h4>
+            <h4 className="font-bold mt-0">{name}</h4>
             <div>{category}</div>
           </div>
         </div>
         <Divider className="m-0" />
-        <div className="p-3">{description}</div>
+        <div className="p-3">{formattedDescription}</div>
       </div>
     </Grid>
   );
--- src/templates/ProjectPage.tsx
+++ src/templates/ProjectPage.tsx
@@ -1,12 +1,38 @@
 import Header from "../components/Header";
+import InputBase from "@mui/material/InputBase";
 import ProjectGrid from "../components/ProjectGrid";
+import React, { useCallback, useEffect, useMemo } from "react";
+import SearchIcon from "@mui/icons-material/Search";
+import { Search } from "@mui/icons-material";
+import { withController } from "../lib/withContoller";
+import throttle from "lodash/throttle";
+import filter from "lodash/fp/filter";
 
 interface Props {
   projectData: any;
 }
 
-export default function ProjectPageTemplate(props: Props) {
+function useController(props: Props) {
   const { projectData } = props;
+  const [search, setSearch] = React.useState("");
+  const [filteredData, setFilteredData] = React.useState<any>(projectData);
+
+  const handleSearch = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value.toLowerCase());
+  };
+
+  useEffect(() => {
+    setFilteredData(filterDataBySearch(projectData, search));
+  }, [search, projectData]);
+
+  return {
+    ...props,
+    handleSearch: throttle(handleSearch, 500),
+    projectData: filteredData,
+  };
+}
+function ProjectPageTemplate(props: ReturnType<typeof useController>) {
+  const { projectData, handleSearch } = props;
 
   console.log(projectData);
 
@@ -15,9 +41,31 @@ export default function ProjectPageTemplate(props: Props) {
       <Header />
       <div className="prose max-w-none max-w-7xl m-auto p-10">
         <h1>Project Page</h1>
+        <div>
+          <Search>
+            <SearchIcon />
+          </Search>
+          <InputBase placeholder="Search anything…" onChange={handleSearch} />
+        </div>
         <p>{projectData["project_name"]}</p>
         <ProjectGrid projectData={projectData} />
       </div>
     </div>
   );
+}
+
+export default withController(ProjectPageTemplate, useController);
+
+function filterDataBySearch(projectData: any, search: string) {
+  if (!search) return projectData;
+
+  return filter((item: any) => {
+    const text = (
+      item["project_name"] +
+      " " +
+      item["description_short_value_proposition_in_a_tweet"]
+    ).toLowerCase();
+
+    return text.includes(search);
+  }, projectData);
 }
