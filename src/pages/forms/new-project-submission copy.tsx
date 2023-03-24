ce1a546598d8b72baa6720e410431f5599c9cf83
Andrew Garde Joia <drewdrewthis@gmail.com>
2023-03-24 14:57:57 +0100

    Update header menu (#3)

---
+++ src/pages/forms/new-project-submission copy.tsx
@@ -0,0 +1,7 @@
+import GoogleFormPageTemplate from "../../templates/GoogleFormPage";
+
+export default function ApiApplication() {
+  return (
+    <GoogleFormPageTemplate iframeSrc="https://docs.google.com/forms/d/e/1FAIpQLSekFpZpdRC8mGFkDoJREahh9OTrwjwK0n2XU1uVkLwR7qQHsw/viewform?embedded=true" />
+  );
+}
