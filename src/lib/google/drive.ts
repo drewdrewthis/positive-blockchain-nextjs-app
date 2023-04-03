cc906a24b45c95b99d70f04634f7d0180b8cd0b6
drewdrew <drewdrewthis@gmail.com>
2023-04-03 13:23:27 +0200

    Set up pages and redirects

--- src/lib/google/drive.ts
+++ src/lib/google/drive.ts
@@ -1,76 +1,5 @@
 import * as google from "@googleapis/drive";
 import { getAuth } from "./auth";
-import fs from "fs";
-
-/**
- * Upload a file from the file system to Google Drive using the Google Drive API.
- */
-export async function uploadFile() {
-  const auth = getAuth();
-  const drive = google.drive({ version: "v3", auth });
-
-  const requestBody = {
-    name: "pb-database.csv",
-    // fields: "id",
-    parents: ["1tMmVWWxe_ZREIRkw9AaQWawe1vE35VTf"],
-    role: "reader",
-    type: "anyone",
-  };
-
-  const media = {
-    uploadType: "media",
-    mimeType: "text/csv",
-    body: fs.createReadStream("src/files/positive-blockchain-database.csv"),
-  };
-
-  try {
-    const file = await drive.files.create({
-      includePermissionsForView: "published",
-      requestBody: {
-        name: "pb-database.csv",
-        parents: ["1tMmVWWxe_ZREIRkw9AaQWawe1vE35VTf"],
-      },
-      media: media,
-    });
-
-    console.log("File Id:", file.data.id, file);
-
-    await drive.permissions.create({
-      fileId: file.data.id as string,
-      requestBody: {
-        role: "reader",
-        type: "anyone",
-      },
-    });
-
-    return file.data.id;
-  } catch (err) {
-    console.error(err);
-    throw err;
-  }
-}
-
-export async function getLatestCsvId() {
-  const auth = getAuth();
-  const drive = google.drive({ version: "v3", auth });
-
-  const query = "name contains 'pb_database_download'";
-
-  const response = await drive.files.list({
-    q: query,
-    fields: "files(id, name, parents)",
-  });
-
-  const files = response.data.files;
-
-  if (files?.length) {
-    const file = files[0];
-    console.log("Latest CSV file:", file);
-    return file.id;
-  } else {
-    console.log("No files found.");
-  }
-}
 
 export async function getLatestCsvInfo(): Promise<{
   id: string;
@@ -96,4 +25,9 @@ export async function getLatestCsvInfo(): Promise<{
   } else {
     console.log("No files found.");
   }
+}
+
+export async function getLatestCsvId() {
+  const info = await getLatestCsvInfo();
+  return info?.id;
 }
---
+++ src/pages/download-csv.tsx
@@ -0,0 +1,78 @@
+import Footer from "@/templates/partials/Footer";
+import Header from "@/templates/partials/Header";
+import { Button } from "@mui/material";
+import { getLatestCsvInfo } from "../lib/google/drive";
+import { GetServerSideProps } from "next";
+
+export default function DownloadCsvPage(props: {
+  id: string;
+  name: string;
+  parents: string[];
+  downloadLink: string;
+}) {
+  const { downloadLink } = props;
+
+  return (
+    <div
+      className="flex flex-col h-full"
+      style={{
+        height: "100vh",
+      }}
+    >
+      <Header />
+      <div className="h-full">
+        <div className="container mx-auto text-center my-10 ">
+          <div className="mb-5">
+            <p className="text-2xl font-bold">
+              Thank you for interest in PositiveBlockchain!
+            </p>
+            <p className="">
+              Please click the button below to download the CSV file.
+            </p>
+          </div>
+          <Button className="btn btn-blue bg-blue-500" variant="contained">
+            <a download="positive-blockchain-database.csv" href={downloadLink}>
+              Download CSV
+            </a>
+          </Button>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const info = await getLatestCsvInfo();
+  const parsedInfo = parseFileContents(info);
+
+  return {
+    props: {
+      ...parsedInfo,
+    },
+  };
+};
+
+function parseFileContents(
+  fileInfo: {
+    id: string;
+    name: string;
+    parents: string[];
+  } | void
+) {
+  if (!fileInfo) return {};
+
+  const timestamp = fileInfo?.name
+    ?.split("pb_database_download-")[1]
+    .split(".csv")[0];
+
+  return {
+    ...fileInfo,
+    timestamp,
+    downloadLink: createDownloadLink(fileInfo?.id),
+  };
+}
+
+function createDownloadLink(id: string) {
+  return `https://drive.google.com/uc?id=${id}&export=`;
+}
