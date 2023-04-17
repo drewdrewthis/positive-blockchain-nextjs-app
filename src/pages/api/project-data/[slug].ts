f913e0d70b1431960b2b31d9f3616c3f7a09b319
drewdrew <drewdrewthis@gmail.com>
2023-04-17 16:07:35 +0200

    Update individual page caching

--- src/pages/api/project-data/[slug].ts
+++ src/pages/api/project-data/[slug].ts
@@ -1,11 +1,21 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { fetchSingleProjectData } from "@/lib/google";
+import { config } from "@/configuration";
+
+const {
+  projects: { CACHE_TTL },
+} = config.constants;
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
+  res.setHeader(
+    "Cache-Control",
+    `public, s-maxage=${CACHE_TTL}, stale-while-revalidate`
+  );
+
   const { slug } = req.query;
   const projectData = await fetchSingleProjectData(slug as string);
   res.status(200).json({ data: projectData });
