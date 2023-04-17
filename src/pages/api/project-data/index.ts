920ccb865ece6e5bdb048ee44abf139eb7a2eaf3
drewdrew <drewdrewthis@gmail.com>
2023-04-17 14:21:21 +0200

    Update api caching

--- src/pages/api/project-data/index.ts
+++ src/pages/api/project-data/index.ts
@@ -7,5 +7,8 @@ export default async function handler(
   res: NextApiResponse<any>
 ) {
   const projectData = await fetchProjectData();
+
+  res.setHeader("Cache-Control", "s-maxage=60 , stale-while-revalidate");
+
   res.status(200).json({ data: projectData });
 }
