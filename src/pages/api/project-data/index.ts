d6b1f1c442387806dab93110fc58736abf9b8b0a
drewdrew <drewdrewthis@gmail.com>
2023-04-26 11:26:36 +0200

    Try to remove cache

--- src/pages/api/project-data/index.ts
+++ src/pages/api/project-data/index.ts
@@ -1,37 +1,16 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { fetchProjectData } from "@/lib/google";
-import { isValidApiKeyAsync } from "@/lib/apiKeyChecker";
 
-async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
-  const key = req.headers["x-api-key"] as string;
-
-  if (!(await isValidApiKeyAsync(key))) {
-    res.status(401).json({ error: "Unauthorized: Bad Api Key" });
-    return;
+async function handler(_req: NextApiRequest, res: NextApiResponse<any>) {
+  try {
+    const projectData = await fetchProjectData();
+    res.setHeader("Cache-Control", "s-maxage=60 , stale-while-revalidate");
+    res.status(200).json({ data: projectData });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error });
   }
-
-  const projectData = await fetchProjectData();
-
-  res.setHeader("Cache-Control", "s-maxage=60 , stale-while-revalidate");
-
-  res.status(200).json({ data: projectData });
 }
 
-const allowCors = (fn: any) => async (req: any, res: any) => {
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader("Access-Control-Allow-Methods", "GET");
-
-  res.setHeader("Access-Control-Allow-Headers", "x-api-key");
-
-  if (req.method === "OPTIONS") {
-    res.status(200).end();
-    return;
-  }
-  return await fn(req, res);
-};
-
-export default allowCors(handler);
+export default handler;
---
+++ src/pages/api/v1/projects.ts
@@ -0,0 +1,79 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from "next";
+import { isValidApiKeyAsync } from "@/lib/apiKeyChecker";
+import Routes from "@/lib/Routes";
+import type { Project } from "@/types";
+import isUndefined from "lodash/fp/isUndefined";
+
+/**
+ * This is the API V1 endpoint for the project data.
+ */
+async function handler(req: NextApiRequest, res: NextApiResponse<any>) {
+  const key = req.headers["x-api-key"] as string;
+
+  try {
+    if (!(await isValidApiKeyAsync(key))) {
+      res.status(401).json({ error: "Unauthorized: Bad Api Key" });
+      return;
+    }
+  } catch (e) {
+    res.status(500).json({ error: e });
+    return;
+  }
+
+  try {
+    const projectData = await fetchAllProjectData();
+    const paginationOptions = parsePaginationOptions(req.query);
+    const pagedData = paginateData(projectData.data, paginationOptions);
+    res.status(200).json({ data: pagedData });
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: e });
+  }
+}
+
+const allowCors = (fn: any) => async (req: any, res: any) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "x-api-key, X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+  );
+
+  if (req.method === "OPTIONS") {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+export default allowCors(handler);
+
+async function fetchAllProjectData() {
+  const url = `${Routes.getBaseUrl()}/api/project-data`;
+  // Use the other API endpoint to fetch the data
+  // to leverage the caching mechanism
+  return fetch(url).then((res) => res.json());
+}
+
+type PaginationOptions = {
+  limit: number;
+  offset: number;
+};
+function parsePaginationOptions(
+  query: NextApiRequest["query"]
+): PaginationOptions {
+  const limit = parseInt(query.limit as string, 10);
+  const offset = parseInt(query.offset as string, 10);
+
+  return {
+    limit: isUndefined(limit) ? 10 : limit,
+    offset: isUndefined(offset) ? 0 : offset,
+  };
+}
+function paginateData(data: Project[], options: PaginationOptions): Project[] {
+  const startIndex = options.offset;
+  const endIndex = startIndex + options.limit;
+  return data.slice(startIndex, endIndex);
+}
