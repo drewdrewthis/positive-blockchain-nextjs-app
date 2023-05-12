b283e7a7ac709cdcb56b2f6fd2949cdcf96b6a86
drewdrew <drewdrewthis@gmail.com>
2023-05-12 09:59:02 -0600

    Use OpenSans

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,13 +2,29 @@ import type { AppProps } from "next/app";
 import "@/styles/globals.css";
 import createEmotionCache from "../utils/createEmotionCache";
 import { CacheProvider } from "@emotion/react";
+import { Open_Sans } from "next/font/google";
+import { ThemeProvider, createTheme } from "@mui/material";
 
+// If loading a variable font, you don't need to specify the font weight
+const openSans = Open_Sans({ subsets: ["latin"] });
 const clientSideEmotionCache = createEmotionCache();
+const theme = createTheme({
+  typography: {
+    fontFamily: openSans.style.fontFamily,
+  },
+});
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <CacheProvider value={clientSideEmotionCache}>
-      <Component {...pageProps} />
+      <style jsx global>{`
+        html {
+          font-family: ${openSans.style.fontFamily};
+        }
+      `}</style>
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </CacheProvider>
   );
 }
